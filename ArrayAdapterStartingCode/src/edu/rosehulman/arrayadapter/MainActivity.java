b3ea8eef06a4151ad6c9e989d293fe643392e3bd
Matt Boutell <boutell@gmail.com>
2014-06-25 17:27:15 -0400

    Finished starting code

--- ArrayAdapterStartingCode/src/edu/rosehulman/arrayadapter/MainActivity.java
+++ ArrayAdapterStartingCode/src/edu/rosehulman/arrayadapter/MainActivity.java
@@ -9,27 +9,24 @@ import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
-	
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_main);
-	
-	ArrayList<StateBird> birds = new ArrayList<StateBird>();
-	birds.add(new StateBird("Cardinal", "Indiana"));
-	birds.add(new StateBird("Willow Goldfinch", "Washington"));
-	birds.add(new StateBird("Valley Quail", "California"));
-	birds.add(new StateBird("Cardinal", "Illinois"));
-	birds.add(new StateBird("American Robin", "Michigan"));
-	birds.add(new StateBird("Chickadee", "Massachusetts"));
-	
-	ArrayAdapter<StateBird> adapter = new ArrayAdapter<StateBird>(this, android.R.layout.simple_list_item_1, birds);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-//		BirdAdapter adapter = new BirdAdapter(this, birds); 
-	
-	
-	ListView listView = (ListView)findViewById(R.id.list_view);
-	listView.setAdapter(adapter);
-	
-}
+		// Add some birds
+		ArrayList<StateBird> birds = new ArrayList<StateBird>();
+		birds.add(new StateBird("Cardinal", "Indiana"));
+		birds.add(new StateBird("Willow Goldfinch", "Washington"));
+		birds.add(new StateBird("Valley Quail", "California"));
+		birds.add(new StateBird("Cardinal", "Illinois"));
+		birds.add(new StateBird("American Robin", "Michigan"));
+		birds.add(new StateBird("Chickadee", "Massachusetts"));
+
+		ArrayAdapter<StateBird> adapter = new ArrayAdapter<StateBird>(this,
+				android.R.layout.simple_list_item_1, birds);
+
+		ListView listView = (ListView) findViewById(R.id.list_view);
+		listView.setAdapter(adapter);
+	}
 }
