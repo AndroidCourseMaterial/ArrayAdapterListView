5cf8766ad22275caa193d3e660dcebdd5eda5da7
Matt Boutell <boutell@gmail.com>
2014-06-25 16:50:17 -0400

    Refactored bird class. Cleaned up.

--- ArrayAdapterStartingCode/src/edu/rosehulman/arrayadapter/MainActivity.java
+++ ArrayAdapterStartingCode/src/edu/rosehulman/arrayadapter/MainActivity.java
@@ -9,51 +9,27 @@ import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
-class StateBird {
-	String mName;
-	String mState;
 	
-	public StateBird(String name, String state) {
-		mName = name;
-		mState = state;
-	}
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
 	
-	@Override
-	public String toString() {
-		return mName + " - " + mState;
-	}
-
-
-	public String getName() {
-		return mName;
-	}
+	ArrayList<StateBird> birds = new ArrayList<StateBird>();
+	birds.add(new StateBird("Cardinal", "Indiana"));
+	birds.add(new StateBird("Willow Goldfinch", "Washington"));
+	birds.add(new StateBird("Valley Quail", "California"));
+	birds.add(new StateBird("Cardinal", "Illinois"));
+	birds.add(new StateBird("American Robin", "Michigan"));
+	birds.add(new StateBird("Chickadee", "Massachusetts"));
 	
-	public String getState() {
-		return mState;
-	}
-	
-}
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		ArrayList<StateBird> birds = new ArrayList<StateBird>();
-		birds.add(new StateBird("Cardinal", "Indiana"));
-		birds.add(new StateBird("Willow Goldfinch", "Washington"));
-		birds.add(new StateBird("Valley Quail", "California"));
-		birds.add(new StateBird("Cardinal", "Illinois"));
-		birds.add(new StateBird("American Robin", "Michigan"));
-		birds.add(new StateBird("Chickadee", "Massachusetts"));
-		
-		ArrayAdapter<StateBird> adapter = new ArrayAdapter<StateBird>(this, android.R.layout.simple_list_item_1, birds);
+	ArrayAdapter<StateBird> adapter = new ArrayAdapter<StateBird>(this, android.R.layout.simple_list_item_1, birds);
 
 //		BirdAdapter adapter = new BirdAdapter(this, birds); 
-		
-		
-		ListView listView = (ListView)findViewById(R.id.list_view);
-		listView.setAdapter(adapter);
-		
-	}
+	
+	
+	ListView listView = (ListView)findViewById(R.id.list_view);
+	listView.setAdapter(adapter);
+	
+}
 }
